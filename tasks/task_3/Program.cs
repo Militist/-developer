c688b50bd011a221f738d4b9d4274729fadc2fae
militist <neumpanin@yandex.ru>
2022-10-15 23:01:18 +0300

    Задача №6

---
+++ tasks/task_3/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if(a % 2 == 0)
+{
+    System.Console.WriteLine($"Да, число {a} делится без остатка.");
+}
+else System.Console.WriteLine($"Нет, число {a} является нечётным.");
+
