dd018a074bd079130d63efeeb2845c81b5c51476
militist <neumpanin@yandex.ru>
2022-10-18 15:16:32 +0300

    Задача №13

---
+++ tasks/task_13/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача №13: Напишите программу, которая выводит третью цифру (справа налево) заданного числа или сообщает, 
+//что третьей цифры нет.
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+int count = 0, a3 = 0, num = 0;
+a3 = (a / 100) % 10;
+while(a > 0)
+{
+    num = a % 10;
+    a = a / 10;
+    count++;
+}
+if(count < 3)
+{
+    System.Console.WriteLine("Третьей цифры не существует.");
+}
+else
+{
+    System.Console.WriteLine($"Третья цифра введённого числа - это {a3}");
+}
