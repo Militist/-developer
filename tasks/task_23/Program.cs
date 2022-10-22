297049d7826041f793de4be56c9e9af74f590ad3
militist <neumpanin@yandex.ru>
2022-10-22 17:03:50 +0300

    Задача №23

---
+++ tasks/task_23/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+void CountIt(int num)
+{
+    Console.WriteLine("Число\tКуб");
+    for(int i = 1; i <= num; i++)
+    {
+        int res = i * i * i;
+        Console.WriteLine("число {0}\t{1}", i, res);
+    }
+}
+Console.WriteLine("Введите количество чисел, которое необходимо возвести в третью степень: ");
+int number = int.Parse(Console.ReadLine()!);
+CountIt(number);
