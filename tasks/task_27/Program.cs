815398ef1886f21ee39f467fc3b14aac4856402f
militist <neumpanin@yandex.ru>
2022-11-12 09:41:32 +0300

    Задача №27

---
+++ tasks/task_27/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+int NumAbruf(int zahl)
+{
+    int sum = 0;
+
+    if(zahl < 0)
+    {
+        zahl = zahl * -1;
+    }
+
+    while(zahl > 0)
+    {
+       sum = sum + zahl % 10;
+       zahl = zahl / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("geben Sie eine Zahl ein: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int res = NumAbruf(num);
+Console.WriteLine($"summe der Zahlenziffern: {res}");
