af08bf6041774889fdbe75805420c02807a0e9a0
militist <neumpanin@yandex.ru>
2022-11-12 09:54:51 +0300

    Задача №29

---
+++ tasks/task_29/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] ZeigArray()
+{
+    int[] MeinAarray = new int[8];
+    Random rand = new Random();
+
+    for(int i = 0; i < MeinAarray.Length; i++)
+    {
+        MeinAarray[i] = rand.Next(1, 100);
+    }
+    return MeinAarray;
+}
+
+int[] newMeinArray = ZeigArray();
+
+Console.Write(String.Join(", ", newMeinArray));
