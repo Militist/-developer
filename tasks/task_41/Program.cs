eae545c0856d13b7b7b9f1e5a18a6e5d0e90daba
militist <neumpanin@yandex.ru>
2022-11-23 10:52:51 +0300

    Задача 41

---
+++ tasks/task_41/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+int[] GetArray()
+{
+   Console.Write("Введите необходимую длину массива: ");
+   int wert = int.Parse(Console.ReadLine()!);
+
+   int[] MeinArray = new int[wert];
+
+   for(int i = 0; i < MeinArray.Length; i++)
+   {
+        Console.Write($"Введите число под индексом {i + 1}: ");
+        MeinArray[i] = int.Parse(Console.ReadLine()!);
+   }
+   Arraydrucken(MeinArray);
+   return MeinArray;
+}
+
+void Arraydrucken(int[] array)
+{
+    Console.WriteLine(String.Join(", ", array));
+}
+
+int ZahlenSuchen(int[] array)
+{
+    int zaehler = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            zaehler++;
+        }
+    }
+    return zaehler;
+}
+
+int[] NeuArray = GetArray();
+int res = ZahlenSuchen(NeuArray);
+Console.WriteLine($"Количество введённых положительных чисел: {res}");
